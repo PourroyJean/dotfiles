551b097e8873fe391ed894ef8ea0bba1d0c622d9
Your Name <jean.pourroy@gmail.com>
2022-10-10 18:45:43 +0300

    new install summary

--- install.sh
+++ install.sh
@@ -5,32 +5,76 @@ PATH_DOT_FILE=`pwd`
 CFG_EMACS_PATH=~/.emacs
 CFG_TMUX_PATH=~/.tmux.conf
 
-is_INSTALL_FZF=true
+#FLAG
+is_FZF_installed=false
+is_TREE_installed=false
+is_EMACS_installed=false
+is_TMUX_installed=false
 
-echo "test -s $PATH_DOT_FILE/bashrc_custom       && . $PATH_DOT_FILE/bashrc_custom       || true" >> ~/.bashrc
-echo "test -s $PATH_DOT_FILE/bash_aliases_custom && . $PATH_DOT_FILE/bash_aliases_custom || true" >> ~/.bashrc
+#ADD THE TWO CUSTOME FILES TO THE EXISTING ONE
+#Check if already updated with our custom files      -- before sourcing the file we test if it exists
+grep -q $PATH_DOT_FILE/bashrc_custom       ~/.bashrc || echo "test -s $PATH_DOT_FILE/bashrc_custom       && . $PATH_DOT_FILE/bashrc_custom       || true" >> ~/.bashrc
+grep -q $PATH_DOT_FILE/bash_aliases_custom ~/.bashrc || echo "test -s $PATH_DOT_FILE/bash_aliases_custom && . $PATH_DOT_FILE/bash_aliases_custom || true" >> ~/.bashrc
 
-
-
-# We check if the file already exists: we save it as backup.old
-if [[ -f "$CFG_EMACS_PATH" ]]; then
-    mv $CFG_EMACS_PATH "${CFG_EMACS_PATH}.old"
+# -- EMACS --
+if [ -x "$(command -v emacs)" ]; then
+  # We check if the file already exists: we save it as backup.old
+  if [[ -f "$CFG_EMACS_PATH" ]]; then
+      mv $CFG_EMACS_PATH "${CFG_EMACS_PATH}.old"
+  fi
+  ln -s $PATH_DOT_FILE/emacs_custom     $CFG_EMACS_PATH
+  is_EMACS_installed=true
+else
+  echo "emacs is not installed"
 fi
-ln -s $PATH_DOT_FILE/emacs_custom     $CFG_EMACS_PATH
 
 
-if [[ -f "$CFG_TMUX_PATH" ]]; then
-    mv $CFG_TMUX_PATH "${CFG_TMUX_PATH}.old"
+# -- TMUX --
+if [ -x "$(command -v tmux)" ]; then
+  # We check if the file already exists: we save it as backup.old
+  if [[ -f "$CFG_TMUX_PATH" ]]; then
+     mv $CFG_TMUX_PATH "${CFG_TMUX_PATH}.old"
+  fi
+  ln -s $PATH_DOT_FILE/tmux.conf_custom $CFG_TMUX_PATH
+  is_TMUX_installed=true
+else
+  echo "tmux is not installed"
 fi
-ln -s $PATH_DOT_FILE/tmux.conf_custom $CFG_TMUX_PATH
 
 
-#FZF - https://github.com/junegunn/fzf#using-git
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+# -- FZF --
+#Check https://github.com/junegunn/fzf#using-git
+if ! [ -x "$(command -v fzf)" ]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+fi
+[ -x "$(command -v fzf)" ] && is_FZF_installed=true
 
+
+#-- TREE --
+if ! [ -x "$(command -v tree)" ]; then
+    echo "Installing tree command : ";
+    [ -d tmp ] || mkdir tmp
+    cd tmp
+    wget https://mama.indstate.edu/users/ice/tree/src/tree-2.0.4.tgz
+    ex tree-2.0.4.tgz
+    cd tree-2.0.4/
+    make
+    [ -d ~/.local/bin ] || mkdir -p ~/.local/bin
+    cp tree ~/.local/bin/
+    cd ../..
+    echo "Please add .local/bin/ to your PATH ..."
+    PATH=.local/bin/:$PATH
+fi
+[ -x "$(command -v tree)" ] && is_TREE_installed=true
+
+
+echo "+ Summary of the installation : "
+echo "   - fzf       $is_FZF_installed"
+echo "   - tree      $is_TREE_installed"
+echo "   - emacs     $is_EMACS_installed"
+echo "   - tmux      $is_TMUX_installed"
 
 emacs $PATH_DOT_FILE/bashrc_custom #EDIT SERVER-NAME
 
-# CDD command to move to DOTFILE location
-echo -e "\nalias CDD='cd ${PATH_DOT_FILE}'" >> ~/.bashrc
+
