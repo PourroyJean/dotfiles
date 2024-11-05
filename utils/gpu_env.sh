778566a4307a66fc3cc47f55a7109a9e40563e61
Your Name <jean.pourroy@gmail.com>
2024-11-05 17:49:56 +0200

    zsh GPU script

--- utils/gpu_env.sh
+++ utils/gpu_env.sh
@@ -1,37 +1,34 @@
+#!/bin/zsh
 ulimit -c unlimited
 ulimit -s unlimited
 
 # Store tracing and disable (module is *way* too verbose)
-{ tracing_=${-//[^x]/}; set +x; } 2>/dev/null
+tracing_=${options[xtrace]:+"x"}
+set +x 2>/dev/null
 
 module_load() {
   echo "+ module load $1"
-  if [ "${2:-""}" == "ECBUNDLE_CONFIGURE_ONLY" ]; then
-    if [ -n "${ECBUNDLE_CONFIGURE:-""}" ]; then
-      module load $1
+  if [[ "${2:-""}" == "ECBUNDLE_CONFIGURE_ONLY" ]]; then
+    if [[ -n "${ECBUNDLE_CONFIGURE:-""}" ]]; then
+      module load "$1"
     else
       echo " WARNING: Module $1 not loaded (only during configuration)"
     fi
   else
-    module load $1
+    module load "$1"
   fi
 }
+
 module_unload() {
   echo "+ module unload $1"
-  module unload $1
+  module unload "$1"
 }
-
 
 module_load PrgEnv-cray
 module_load craype-x86-trento
 module_load craype-accel-amd-gfx90a
 module_load rocm
+
 export MPICH_GPU_SUPPORT_ENABLED=1
 
-
 module list 2>&1
-
-
-# Restore tracing to stored setting
-{ if [[ -n "$tracing_" ]]; then set -x; else set +x; fi } 2>/dev/null
-
